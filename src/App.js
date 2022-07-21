68390de420a7f4e6886b67f2bbdafe6c16efde2b
Shyby Mallanveliyil <shybyhamza@gmail.com>
2022-07-20 22:52:40 -0500

    Mern Pedro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name,setName] = useState("")
+  const [age,setAge] = useState("")
+  const [username,setUsername] = useState("")
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    })
+  }, []);
+
+  const createUser = () => {
+    Axios.post('http://localhost:3001/createUser', {name:"", age: 0, username: "",}).then((response) => {
+      alert('User created');
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='userDisplay'>
+        {listOfUsers.map((user) => {
+          return (
+            <div> 
+              <h1> Name: {user.name}</h1>
+              <h1> Age: {user.age}</h1>
+              <h1> Username: {user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        <input type='text' placeholder='name' onChange={(event) => {setName(event.target.value);}}/>
+        <input type='text' placeholder='age' />
+        <input type='text' placeholder='username' />
+        <button onClick={createUser} >Create user</button>
+      </div>
     </div>
   );
 }
